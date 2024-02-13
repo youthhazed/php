9e94016196f45e851932d4ef904f2cc935030502
youthhazed <term7ina3tor@gmail.com>
2024-02-13 12:47:46 +0300

    lab1 done

---
+++ lab1/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Тестируем PHP</title>
+</head>
+<body>
+    <? echo '<p>Hello, World!</p>'; ?>
+</body>
+</html>
